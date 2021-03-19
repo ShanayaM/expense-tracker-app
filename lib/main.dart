aa2133d5bc86319fa0944d5b81e369d3e6ddb546
ShanayaM <msdec.ict@gmail.com>
2021-03-19 13:44:07 +0100

    Added CircularProgressIndicator for authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'authentication_repository.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,13 +23,16 @@ class LoginPage extends StatefulWidget {
   LoginPage({Key? key, required this.title}) : super(key: key);
 
   final String? title;
+  final AuthRepository authRepository = UserAuthentication();
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool waiting = false;
   TextStyle textStyle = TextStyle(fontSize: 20.0);
+
   @override
   Widget build(BuildContext context) {
     final emailField = TextField(
@@ -49,22 +54,6 @@ class _LoginPageState extends State<LoginPage> {
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
 
-    final loginBtn = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      child: MaterialButton(
-//         minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        elevation: 5.0,
-        color: Colors.green,
-        onPressed: () {},
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: textStyle.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title!),
@@ -72,28 +61,70 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: Container(
           color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(height: 45),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(height: 35.0),
-                loginBtn,
-                SizedBox(
-                  height: 20,
+          child: waiting
+              ? CircularProgressIndicator()
+              : Padding(
+                  padding: const EdgeInsets.all(36.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 45),
+                      emailField,
+                      SizedBox(height: 25.0),
+                      passwordField,
+                      SizedBox(height: 35.0),
+                      Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: MaterialButton(
+                          minWidth: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                          elevation: 5.0,
+                          color: Colors.green,
+                          onPressed: () async {
+                            setState(() {
+                              waiting = true;
+                            });
+                            final result = await widget.authRepository.login();
+                            setState(() {
+                              waiting = false;
+                            });
+                            if (result) {
+                              Fluttertoast.showToast(
+                                  msg: "Welcome back!",
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.CENTER,
+                                  backgroundColor: Colors.grey,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0);
+                            } else {
+                              Fluttertoast.showToast(
+                                  msg: "Please try again...",
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.CENTER,
+                                  backgroundColor: Colors.grey,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0);
+                            }
+                          },
+                          child: Text("Login",
+                              textAlign: TextAlign.center,
+                              style: textStyle.copyWith(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text('New User? Create an Account!')
+                    ],
+                  ),
                 ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Text('New User? Create an Account!')
-              ],
-            ),
-          ),
         ),
       ),
     );

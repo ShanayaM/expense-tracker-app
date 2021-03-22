be5bf6047d7d5e360b29f5ac8c07dd334e378fb2
ShanayaM <msdec.ict@gmail.com>
2021-03-22 19:08:00 +0100

    Fixed issues with pie chart

--- lib/chart.dart
+++ lib/chart.dart
@@ -17,6 +17,14 @@ class Chart extends HookWidget {
 
       double totalSum = 0.0;
 
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
       return {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum
@@ -33,6 +41,11 @@ class Chart extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dataMap = <String, double>{};
+    for (final map in groupedTransactionValues) {
+      dataMap[map['day'].toString()] = map['amount'] as double;
+    }
+
     List<Color> colorList = [
       Colors.red,
       Colors.green,
@@ -52,43 +65,36 @@ class Chart extends HookWidget {
         padding: EdgeInsets.all(10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: groupedTransactionValues.map(
-            (data) {
-              Map<String, double> dataMap = {
-                data['day'].toString(): maxSpending
-              };
-              return Flexible(
-                fit: FlexFit.tight,
-                child: PieChart(
-                  dataMap: dataMap,
-                  animationDuration: Duration(milliseconds: 800),
-                  chartLegendSpacing: 32,
-                  chartRadius: MediaQuery.of(context).size.width / 3.2,
-                  colorList: colorList,
-                  centerText: maxSpending.toString(),
-                  initialAngleInDegree: 0,
-                  chartType: ChartType.disc,
-                  ringStrokeWidth: 32,
-                  legendOptions: LegendOptions(
-                    showLegendsInRow: false,
-                    legendPosition: LegendPosition.right,
-                    showLegends: true,
-                    legendShape: BoxShape.circle,
-                    legendTextStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  chartValuesOptions: ChartValuesOptions(
-                    showChartValueBackground: true,
-                    showChartValues: true,
-                    showChartValuesInPercentage: false,
-                    showChartValuesOutside: false,
-                    decimalPlaces: 1,
+          children: [
+            Flexible(
+              fit: FlexFit.tight,
+              child: PieChart(
+                dataMap: dataMap,
+                chartLegendSpacing: 30,
+                chartRadius: MediaQuery.of(context).size.width / 3.2,
+                colorList: colorList,
+                initialAngleInDegree: 0,
+                chartType: ChartType.disc,
+                ringStrokeWidth: 32,
+                legendOptions: LegendOptions(
+                  showLegendsInRow: false,
+                  legendPosition: LegendPosition.right,
+                  showLegends: true,
+                  legendShape: BoxShape.circle,
+                  legendTextStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              );
-            },
-          ).toList(),
+                chartValuesOptions: ChartValuesOptions(
+                  showChartValueBackground: true,
+                  showChartValues: true,
+                  showChartValuesInPercentage: false,
+                  showChartValuesOutside: false,
+                  decimalPlaces: 1,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
